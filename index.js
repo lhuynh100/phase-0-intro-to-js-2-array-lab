0e243c7655e495158dd27fd2b64c84b3e68508b3
Le Huynh <lhuynh100@gmail.com>
2022-10-03 14:29:26 -0500

    commit message

--- index.js
+++ index.js
@@ -1,1 +1,108 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+    return cats;
+}
+
+function  destructivelyPrependCat(name) {
+    cats.unshift(name);
+    return cats;
+}
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop(name);
+    return cats;
+}
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift(name);
+    return cats;
+}
+function appendCat(name) {
+    const coolCats = [...cats, name];
+    return coolCats;
+}
+
+function prependCat(name) {
+    const coolCats = [name, ...cats];
+    return coolCats;
+}
+
+function removeLastCat(name) {
+    const kats = cats.slice(0, cats.length -1);
+    return kats;
+}
+
+
+function removeFirstCat(name) {
+    const kats = cats.slice(1);
+    return kats;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const cats = ["Milo", "Otis", "Garfield"];
+
+// function destructivelyAppendCat(name) {
+//     cats.push(name);
+//     return cats;
+// }
+
+// function destructivelyPrependCat(name) {
+//     cats.unshift(name);
+//     return cats;
+// }
+
+// function destructivelyRemoveLastCat(name) {
+//     cats.pop(name);
+//     return cats;
+// }
+
+// function destructivelyRemoveFirstCat(name) {
+//     cats.shift(name);
+//     return cats;
+// }
+
+// function appendCat(name) {
+//     const name_1 = [...cats, name];
+//     return name_1;
+// }
+
+// function prependCat(name){
+//     const name_2 = [name, ...cats];
+//     return name_2;
+// }
+
+// function removeLastCat() {
+//     const kats = cats.slice(0, cats.length-1);
+//     return kats;
+// }
+
+// function removeFirstCat() {
+//     const kats = cats.slice(1);
+//     return kats;
+// }
